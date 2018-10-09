77d72a26caf76216baca34803c634996c2061144
Max Coplan <maxcoplan@maxs-mbp.towson.edu>
2018-10-09 09:31:45 -0400

    Initial Commit

---
+++ WeatherBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WeatherBar
+//
+//  Created by Max Coplan on 10/9/18.
+//  Copyright © 2018 Max Coplan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
